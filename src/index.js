2b8be7ecab123fe3dbe35bedb7bbd8232e799aa0
De-Cay <dushik@live.com>
2021-10-03 10:57:21 +0530

    logo added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import home from './components/home';
+import css from './styles/main.css';
 import contact from './components/contact';
 import menu from './components/menu';
+import logo from './static/images/logo.jpg';
 
 
 const $root = document.getElementById('root');
-$root.textContent = home();
+const $logoDiv = document.createElement('div');
+$root.appendChild($logoDiv);
+const $logo = document.createElement('img');
+$logo.src = logo;
+$logo.alt = 'logo';
+$logo.className = 'logo';
+$logoDiv.appendChild($logo);
